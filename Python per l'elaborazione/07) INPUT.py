221aa070bd5f4979e962b7b951a2607fb4f43b4d
Federico Mariconti <142414161+federico-mariconti@users.noreply.github.com>
2024-12-09 15:35:30 +0100

    Delete Python per l'elaborazione directory

--- Python per l'elaborazione/07) INPUT.py
+++
@@ -1,8 +0,0 @@
-nome=input("come ti chiami? ")
-annonascita=int(input("in che anno sei nato? "))
-eta=2024-annonascita
-peso=int(int(input("quanto pesi?"))*0.5)
-
-print("Ciao", nome,",", eta, "anni",end=", ")
-print("sulla luna peseresti", peso)
-print(f"Ciao, {nome} la tua età è di {eta} anni e sulla luna peseresti {peso} Kg")

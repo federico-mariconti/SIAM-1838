6669f42c775f5c1291e24b3a6cec6258cf9d1d72
Federico Mariconti <142414161+federico-mariconti@users.noreply.github.com>
2024-12-12 14:12:15 +0100

    Add files via upload

---
+++ Python per l'elaborazione dei Big Data/17) WHILE INPUT.py
@@ -0,0 +1,5 @@
+numero=-1 #metto -1 perchè se avessi 0 il programma avrebbe già finito essendoci il diverso da zero nel ciclo while
+
+while(numero !=0): #"!="--> significa diverso
+    numero=int(input('dammi un numero (0=STOP): '))
+    print (numero)
